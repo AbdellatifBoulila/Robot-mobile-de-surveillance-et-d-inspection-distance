8b46e42c7505861cd478ce7b2a4c3fbac92fa3e3
AbdellatifBoulila <abdellatif.boulila@univ-bouira.dz>
2026-06-15 13:42:15 +0100

    Add files via upload

---
+++ mainC.cpp
@@ -0,0 +1,33 @@
+#define THRESHOLD_CM 7
+#define SIDE_AVOID_CM 4
+#define ALERT_ZONE_CM 9
+
+#define MODE_AUTO   0
+#define MODE_MANUAL 1
+
+// Prototypes
+...
+
+// Delay
+...
+
+// GPIO
+...
+
+// USART
+...
+
+// IR Sensors
+...
+
+// Ultrasonic
+...
+
+// Motor Control
+...
+
+// Cliff Escape
+...
+
+// Main Loop
+...
